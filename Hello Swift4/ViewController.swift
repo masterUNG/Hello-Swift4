031e7663df7767b48a1188647afc58d417988c89
MasterUNG <phrombutr@gmail.com>
2017-10-14 11:52:10 +0700

    Design UI OK

--- Hello Swift4/ViewController.swift
+++ Hello Swift4/ViewController.swift
@@ -9,11 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Explicit การประกาศตัวแปร แบบกำหนดค่า
+    var nameString: String = ""
+    
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+    }   // Main Function สิ่งแรกที่จะทำงาน
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -21,5 +29,5 @@ class ViewController: UIViewController {
     }
 
 
-}
+}   // Main Class
 
